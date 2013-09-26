3ab5b2754ff764de0826e40bce61996bc2e9b8ef
doron pagot <doronpagot@gmail.com>
2013-09-26 14:18:10 +0300

    write another test for a different destination

--- test/index.js
+++ test/index.js
@@ -15,17 +15,31 @@ var it = Lab.test;
 
 describe('in-its-right-place', function () {
 	before(function(done){
-		Fs.writeFileSync("test/files/case1/source/new.girl-star234.mp3", "this is it");
+		Fs.writeFileSync("test/files/case1/source/new.girl-star234.mp3", "new girl");
 		if (Fs.existsSync("test/files/case1/destination/New Girl/new.girl-star234.mp3")) {
 			Fs.unlinkSync("test/files/case1/destination/New Girl/new.girl-star234.mp3");
 		}
+		Fs.writeFileSync("test/files/case2/source/elementary.0230.stop-star234.mp4", "elementary");
+		if (Fs.existsSync("test/files/case2/destination/elemntary/elementary.0230.stop-star234.mp4")) {
+			Fs.unlinkSync("test/files/case2/destination/elemntary/elementary.0230.stop-star234.mp4");
+		}
 		done();
 	});
-    it('should move files already in the src folder to the dst folder', function (done) {
+
+    it('should new.girl file already in the src folder to the correct dst folder', function (done) {
     	var rightPlace = new InItsRightPlace("test/files/case1/source", "test/files/case1/destination");
         rightPlace.on('rightPlace', function(what){
         	what.src.should.eql(Path.resolve(Path.resolve("test/files/case1/source/new.girl-star234.mp3")));
         	what.dst.should.eql(Path.resolve(Path.resolve("test/files/case1/destination/New Girl/new.girl-star234.mp3")));
+        	done();
+        });
+    });
+
+    it('should elementary file already in the src folder to the correct dst folder', function (done) {
+    	var rightPlace = new InItsRightPlace("test/files/case2/source", "test/files/case2/destination");
+        rightPlace.on('rightPlace', function(what){
+        	what.src.should.eql(Path.resolve(Path.resolve("test/files/case2/source/elementary.0230.stop-star234.mp4")));
+        	what.dst.should.eql(Path.resolve(Path.resolve("test/files/case2/destination/elementary/elementary.0230.stop-star234.mp4")));
         	done();
         });
     });
