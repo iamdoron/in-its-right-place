6491b4270f53a4c47984ad2ec56cbfd91c5a2bb0
doron pagot <doronpagot@gmail.com>
2013-09-26 12:44:17 +0300

    make the test pass

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var InItsRightPlace = require('../lib');
 var Lab = require('lab');
 var Path = require('path');
+var Fs = require('fs');
 var Chai = require('chai');
 Chai.should();
 
@@ -13,12 +14,19 @@ var describe = Lab.experiment;
 var it = Lab.test;
 
 describe('in-its-right-place', function () {
-
+	before(function(done){
+		Fs.writeFileSync("test/files/case1/source/new.girl-star234.mp3", "this is it");
+		if (Fs.existsSync("test/files/case1/destination/New Girl/new.girl-star234.mp3")) {
+			Fs.unlinkSync("test/files/case1/destination/New Girl/new.girl-star234.mp3");
+		}
+		done();
+	});
     it('should move files already in the src folder to the dst folder', function (done) {
-    	var rightPlace = new InItsRightPlace("files/case1/source", "files/case1/destination");
+    	var rightPlace = new InItsRightPlace("test/files/case1/source", "test/files/case1/destination");
         rightPlace.on('rightPlace', function(what){
-        	what.src.should.eql(Path.resolve("files/case1/source/new.girl-star234.mp3"));
-        	what.dst.should.eql(Path.resolve("files/case1/destination/New Girl/new.girl-star234.mp3"));
+        	what.src.should.eql(Path.resolve(Path.resolve("test/files/case1/source/new.girl-star234.mp3")));
+        	what.dst.should.eql(Path.resolve(Path.resolve("test/files/case1/destination/New Girl/new.girl-star234.mp3")));
+        	done();
         });
     });
 });
